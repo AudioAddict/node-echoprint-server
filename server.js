39b7b3ed18752df4edbf88640d38afdb48e784c6
Nick Wilson <wilson.nick@gmail.com>
2015-10-10 22:54:43 -0400

    expose new api endpoints

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
  * Simple HTTP server module
  */
- 
+
 var newrelic = require('newrelic');
 var http = require('http');
 var urlParser = require('url');
@@ -26,27 +26,36 @@ function init() {
   var server = http.createServer(function(req, res) {
     req.start = new Date();
     req.timer = setTimeout(function() { timeout(req, res); }, TIMEOUT);
-    
+
     var url = urlParser.parse(req.url, true);
     var path = url.pathname.split('/', 16);
     var size = 0;
-    
-    if (req.method === 'GET') 
+
+    if (req.method === 'GET')
     {
-      if (path[1] === 'query') {
-        newrelic.setTransactionName('query');
-        return api.query(req, res);
+      try {
+        switch(path[1]) {
+
+        case "query":
+          newrelic.setTransactionName('query');
+          return api.query(req, res);
+          break;
+
+        case "debug":
+          newrelic.setTransactionName('debug');
+          return debug.debugQuery(req, res);
+          break;
+        }
+      } catch(err) {
+        var msg = 'An unknown error has occured: ' + err;
+        log.error(msg);
+        respond(req, res, 500, {error: msg});
       }
-      else if (path[1] === 'debug')
-      {
-        newrelic.setTransactionName('debug');
-        return debug.debugQuery(req, res);
-      }
-    } 
-    else if (req.method === 'POST') 
+    }
+    else if (req.method === 'POST')
     {
       req.body = '';
-      req.on('data', function(data) { 
+      req.on('data', function(data) {
         size += data.length;
         req.body += data;
 
@@ -56,36 +65,58 @@ function init() {
           return respond(req, res, 422, { error: 'POST size cannot exceed 10MB' });
         }
       });
-    
-      req.on('end', function() {        
+
+      req.on('end', function() {
         if (req.body.length === 0) {
 	        return respond(req, res, 422, { error: 'POST query must contain body' });
         }
 
-	      if (path[1] === 'ingest')
-        {
-          newrelic.setTransactionName('ingest');
-          req.body = JSON.parse(req.body);
-          return api.ingest(req, res);
+        try {
+  	      switch(path[1]) {
+
+          case "ingest":
+            newrelic.setTransactionName('ingest');
+            req.body = JSON.parse(req.body);
+            return api.ingest(req, res);
+            break;
+
+          case "ingestAll":
+            newrelic.setTransactionName('ingestAll');
+            req.body = JSON.parse(req.body);
+            return api.ingestAll(req, res);
+            break;
+
+          case "debug":
+            newrelic.setTransactionName('debug');
+            req.body = qs.parse(req.body);
+            return debug.debugQuery(req, res);
+            break;
+
+          case "query":
+            newrelic.setTransactionName('query');
+            req.body = JSON.parse(req.body)
+            return api.query(req, res);
+            break;
+
+          case "queryAll":
+            newrelic.setTransactionName('queryAll');
+            req.body = JSON.parse(req.body);
+            return api.queryAll(req, res);
+            break;
+
+          default:
+            respond(req, res, 404, {error: 'Invalid API endpoint'});
+          }
         }
-        else if (path[1] === 'debug')
-        {
-          newrelic.setTransactionName('debug');
-          req.body = qs.parse(req.body);
-          return debug.debugQuery(req, res);
+        catch (err) {
+          var msg = 'An unknown error has occured: ' + err;
+          log.error(msg);
+          respond(req, res, 500, {error: msg});
         }
-        else if (path[1] === 'query')
-        {
-          newrelic.setTransactionName('query');
-          req.body = JSON.parse(req.body)
-          return api.query(req, res);
-        }
-          
-        respond(req, res, 404, {error: 'Invalid API endpoint'});
       });
       return;
     }
-    
+
     respond(req, res, 404, { error: 'Invalid API endpoint' });
   })
 
@@ -108,7 +139,7 @@ function renderView(req, res, statusCode, view, options, headers) {
       log.error('Failed to render ' + view + ': ' + err);
       return respond(req, res, 500, 'Internal server error', headers);
     }
-    
+
     respond(req, res, 200, html, headers);
   });
 }
@@ -119,34 +150,34 @@ function renderView(req, res, statusCode, view, options, headers) {
 function respond(req, res, statusCode, body, headers) {
   // Destroy the response timeout timer
   clearTimeout(req.timer);
-  
+
   statusCode = statusCode || 200;
-  
+
   if (!headers)
     headers = {};
-  
+
   if (typeof body !== 'string') {
     body = JSON.stringify(body);
     headers['Content-Type'] = 'application/json';
   } else {
     headers['Content-Type'] = 'text/html';
   }
-  
+
   var contentLength = body ? Buffer.byteLength(body, 'utf8') : '-';
   if (body)
     headers['Content-Length'] = contentLength;
-  
+
   var remoteAddress =
     (req.socket && (req.socket.remoteAddress || (req.socket.socket && req.socket.socket.remoteAddress)));
-  
+
   var referrer = req.headers.referer || req.headers.referrer || '';
   if (referrer.length > 128)
     referrer = referrer.substr(0, 128) + ' ...';
-  
+
   var url = req.url;
   if (url.length > 128)
     url = url.substr(0, 128) + ' ...';
-  
+
   log.info(
     remoteAddress +
       ' - - [' + (new Date()).toUTCString() + ']' +
@@ -155,7 +186,7 @@ function respond(req, res, statusCode, body, headers) {
       statusCode + ' ' + contentLength +
       ' "' + referrer +
       '" "' + (req.headers['user-agent'] || '') + '"');
-  
+
   try {
     res.writeHead(200, headers);
     res.end(body);
@@ -172,7 +203,7 @@ function timeout(req, res) {
   var remoteAddress =
     (req.socket && (req.socket.remoteAddress || (req.socket.socket && req.socket.socket.remoteAddress)));
   log.error('Timed out while responding to a request from ' + remoteAddress);
-  
+
   try {
     res.writeHead(503);
     res.end();
